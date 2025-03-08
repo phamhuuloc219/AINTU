6a895aa36f86275927d371cd0491c0b88a18d7ae
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-03-08 18:24:16 +0700

    .

---
+++ ML/data_distribution.py
@@ -0,0 +1,15 @@
+# import numpy
+# import matplotlib.pyplot as plt
+
+# x = numpy.random.uniform(0.0, 5.0, 250)
+
+# plt.hist(x, 5)
+# plt.show()
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.uniform(0.0, 5.0, 100000)
+
+plt.hist(x, 100)
+plt.show()
